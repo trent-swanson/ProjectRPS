cc58b651260d378a80572e2c094580fe1f37d872
AIE\s170999 <sican98@gmail.com>
2018-03-05 15:25:54 +1100

    Made RPSLogic static because it always should've been

--- Assets/Scripts/RPSLogic.cs
+++ Assets/Scripts/RPSLogic.cs
@@ -2,28 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RPSLogic
+public static class RPSLogic
 {
     [HideInInspector]
-    public int[,] OutCome = new int[5,5] { {0,2,1,0,2 }, 
+    public static int[,] OutCome = new int[5,5] { {0,2,1,0,2 }, 
                                            {1,0,2,0,2 },
                                            {2,1,0,0,2 },
                                            {0,0,0,0,0 },
                                            {1,1,1,0,0 }
                                          };
 
-	// Use this for initialization
-	void Start ()
-    {
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 
-    public int ReturnOutcome(int playerOneAction, int PlayerTwoAction)
+
+    public static int ReturnOutcome(int playerOneAction, int PlayerTwoAction)
     {
         return (OutCome[playerOneAction, PlayerTwoAction]);
     }
