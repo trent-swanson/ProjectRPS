aa174fcf957b1e5ffac2961b9e2774d4a87a8288
AIE\s170999 <sican98@gmail.com>
2018-03-05 13:18:24 +1100

    The start of the turn manager

---
+++ Assets/Scripts/Turn Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int MAX_PLAYER_HEALTH;
+    public float MAX_TURN_TIME;
+    private int playerOneHealth;
+    private int playerTwoHealth;
+    private float turnTimer;
+    private List<int> playerOneLastActions;
+    private List<int> playerTwoLastActions;
+}
