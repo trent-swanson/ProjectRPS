1c7dbc53c27e7cca6a4553ed59c241501aea4991
AIE\s170999 <sican98@gmail.com>
2018-03-05 14:18:00 +1100

    More work done on the turn manager,  no functionality yet but it's on the way

--- Assets/Scripts/Turn Manager.cs
+++ Assets/Scripts/Turn Manager.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [HideInInspector]
+    public enum PlayerChoice
+    {
+        ATTACK = 0,
+        LUNGE,
+        PARRY,
+        BLOCK,
+        NOTHING,
+        ITEM_COUNT
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (turnTimer >= 0 || (playerOneNextAction != 0 && playerTwoNextAction != 0)) {
+            turnTimer = MAX_TURN_TIME;
+            //call the RPSlogics functionality to get what happened
+        }
+        else {
+            //check for player inputs and store them
+            turnTimer -= Time.deltaTime;
+        }
 	}
 
     public int MAX_PLAYER_HEALTH;
     public float MAX_TURN_TIME;
+    private float turnTimer;
     private int playerOneHealth;
     private int playerTwoHealth;
-    private float turnTimer;
     private List<int> playerOneLastActions;
     private List<int> playerTwoLastActions;
+    private int playerOneNextAction;
+    private int playerTwoNextAction;
+    private int turnNumber;
 }
