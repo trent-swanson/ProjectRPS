1693b230c23f56a4229431f7c4503e8106b9cfa4
Callan Davies <callan.davies1@gmail.com>
2018-03-05 13:53:23 +1100

    Updated RPSLogic:
    Changed:
     ResolveTurn is now an int function, which returns 0 for a draw, 1 for playerOne, 2 for PlayerTwo and if it returns 3 it has errored and is most likely being called incorrectly.

--- Assets/Scripts/RPSLogic.cs
+++ Assets/Scripts/RPSLogic.cs
@@ -26,7 +26,14 @@ public class RPSLogic : MonoBehaviour
 		
 	}
 
-    public void ResolveTurn(PlayerChoice playerOne, PlayerChoice playerTwo)
+    // ResolveTurn Function:
+    // Parametres: playerOne (the choice of the left player) and playerTwo (the choice of the right player)
+    // Return int:
+      // 0 = this round was a draw
+      // 1 = playerOne won this round
+      // 2 = playerTwo won this round
+      // 3 = ERROR (ignore call)
+    public int ResolveTurn(PlayerChoice playerOne, PlayerChoice playerTwo)
     {
         /* PLAYER ONE ROCK*/
 
@@ -34,24 +41,28 @@ public class RPSLogic : MonoBehaviour
         if (playerOne == PlayerChoice.ROCK && playerTwo == PlayerChoice.ROCK)
         {
             //DRAW
+            return 0;
         }
 
         // Player One = ROCK && Player Two = PAPER
         if (playerOne == PlayerChoice.ROCK && playerTwo == PlayerChoice.PAPER)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = ROCK && Player Two = SCISSORS
         if (playerOne == PlayerChoice.ROCK && playerTwo == PlayerChoice.SCISSORS)
         {
             // playerOne wins round
+            return 1;
         }
 
         // Player One = ROCK && Player Two = NOTHING
         if (playerOne == PlayerChoice.ROCK && playerTwo == PlayerChoice.NOTHING)
         {
             // playerOne wins round
+            return 1;
         }
 
         /* PLAYER ONE PAPER*/
@@ -60,24 +71,28 @@ public class RPSLogic : MonoBehaviour
         if (playerOne == PlayerChoice.PAPER && playerTwo == PlayerChoice.ROCK)
         {
             // playerOne wins round
+            return 1;
         }
 
         // Player One = PAPER && Player Two = PAPER
         if (playerOne == PlayerChoice.PAPER && playerTwo == PlayerChoice.ROCK)
         {
             // DRAW
+            return 0;
         }
 
         // Player One = PAPER && Player Two = SCISSORS
         if (playerOne == PlayerChoice.PAPER && playerTwo == PlayerChoice.SCISSORS)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = PAPER && Player Two = NOTHING
         if (playerOne == PlayerChoice.PAPER && playerTwo == PlayerChoice.NOTHING)
         {
             // playerOne wins round
+            return 1;
         }
 
         /* PLAYER ONE SCISSORS*/
@@ -86,24 +101,28 @@ public class RPSLogic : MonoBehaviour
         if (playerOne == PlayerChoice.SCISSORS && playerTwo == PlayerChoice.ROCK)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = SCISSORS && Player Two = PAPER
         if (playerOne == PlayerChoice.SCISSORS && playerTwo == PlayerChoice.PAPER)
         {
             // playerOne wins round
+            return 1;
         }
 
         // Player One = SCISSORS && Player Two = SCISSORS
         if (playerOne == PlayerChoice.SCISSORS && playerTwo == PlayerChoice.SCISSORS)
         {
             // DRAW
+            return 0;
         }
 
         // Player One = SCISSORS && Player Two = NOTHING
         if (playerOne == PlayerChoice.SCISSORS && playerTwo == PlayerChoice.NOTHING)
         {
             // playerOne wins round
+            return 1;
         }
 
         /* PLAYER ONE NOTHING*/
@@ -112,24 +131,31 @@ public class RPSLogic : MonoBehaviour
         if (playerOne == PlayerChoice.NOTHING && playerTwo == PlayerChoice.ROCK)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = NOTHING && Player Two = PAPER
         if (playerOne == PlayerChoice.NOTHING && playerTwo == PlayerChoice.PAPER)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = NOTHING && Player Two = SCISSORS
         if (playerOne == PlayerChoice.NOTHING && playerTwo == PlayerChoice.SCISSORS)
         {
             // playerOne loses round
+            return 2;
         }
 
         // Player One = NOTHING && Player Two = NOTHING
         if (playerOne == PlayerChoice.NOTHING && playerTwo == PlayerChoice.NOTHING)
         {
             // DRAW
+            return 0;
         }
+
+        // this return should never be called.
+        return 3;
     }
 }
