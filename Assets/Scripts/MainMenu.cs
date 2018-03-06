5198890730a4f460c0b4a5c1f378fee80a5db762
Trent <trentwswanson@gmail.com>
2018-03-06 16:54:11 +1100

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame() {
+		SceneManager.LoadScene ("mainScene");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+	public void Guide() {
+		
+	}
+
+	public void Credits() {
+		
+	}
+}
