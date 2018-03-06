cfda14ffcbc74fc77f54fb72075cc3420335fa7d
Trent <trentwswanson@gmail.com>
2018-03-07 10:30:46 +1100

    Completed End Game Scene

--- Assets/Scripts/WinScreen.cs
+++ Assets/Scripts/WinScreen.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour {
 
-	public Image background;
+	public SpriteRenderer background;
 	public Sprite player1, player2;
+	public float endSceneTime = 15;
+	float timer;
 
 	void Start() {
+		timer = endSceneTime;
 		if (RPSLogic.winner == 1) {
 			background.sprite = player1;
 		} else {
 			background.sprite = player2;
 		}
+	}
+
+	void Update() {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			SceneManager.LoadScene("mainMenu");
+		}
+	}
+
+	public void Menu() {
+		SceneManager.LoadScene("mainMenu");
 	}
 }
